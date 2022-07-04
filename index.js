e13dd6fdb2e89dcc3699f79308e5aed1901fe6a8
GabrielaMendozal <mendozalizethg@gmail.com>
2022-07-03 21:08:25 -0500

    new

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+let cookie = document.querySelector(".cookie");
 
 function mensaje() {
     alert("Loading weather report...");
 }
 
-function definition(cookie) {
+
+let temperatures = document.querySelectorAll('.temperature');
+let temperatureSelector=document.querySelector('#temperatureSelect');
+temperatureSelector.addEventListener('change',cambiarTemperaturas);
+
+function cambiarTemperaturas(event){
+    let temperatureOption =event.target.value;
+
+    if(temperatureOption==="F"){
+        for(let i=0;i<temperatures.length;i++){
+            let convertedTemperature = (parseFloat(temperatures[i].innerText) * 9/5) + 32;
+            temperatures[i].innerText = convertedTemperature.toFixed(0)+"°";
+        }
+    }
+    else{
+        for(let i=0;i<temperatures.length;i++){
+            let convertedTemperature = (parseFloat(temperatures[i].innerText) - 32) * 9/5;
+            temperatures[i].innerText = convertedTemperature.toFixed(0)+"°";
+        }
+    }
+
+}
+
+
+
+
+function closeCookie(){
     cookie.remove();
 }
